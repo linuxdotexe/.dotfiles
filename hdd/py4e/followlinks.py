296f463dec5e26ed31c0246ded3d8c06b776f208
sai nivas mangu <sainonbeat99@gmail.com>
2021-04-20 15:53:25 +0530

    added the programs in my hardrive

---
+++ hdd/py4e/followlinks.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://py4e-data.dr-chuck.net/known_by_Arann.html'
+count = int(input("Enter Count: "))
+position = int(input("Enter Position: "))
+
+names = []
+
+while count > 0:
+    print("retrieving: {0}".format(url))
+    html = urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    atags = soup('a')
+    name = atags[position-1].string
+    names.append(name)
+    url = atags[position-1]['href']
+    count -= 1
+print(names[-1])
