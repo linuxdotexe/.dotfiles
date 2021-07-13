4e16a5d411d08bcf0a54b0159ab9fe718d0d4ac2
sai nivas mangu <sainonbeat99@gmail.com>
2021-07-14 01:04:32 +0530

    ugh

--- awesome/.config/awesome/configuration/keys/global.lua
+++ awesome/.config/awesome/configuration/keys/global.lua
@@ -87,7 +87,7 @@ awful.key(
     {altkey, 'Shift'},
     'e',
     function()
-      awful.spawn.with_shell('xinput enable 13')
+      awful.spawn.with_shell('xinput enable 11')
     end,
     {description = 'enable synaptics touchpad', group = 'touchpad'}
   ),
@@ -95,7 +95,7 @@ awful.key(
     {altkey, 'Shift'},
     'd',
     function()
-      awful.spawn.with_shell('xinput disable 13')
+      awful.spawn.with_shell('xinput disable 11')
     end,
     {description = 'disable synaptics touchpad', group = 'touchpad'}
   ),
