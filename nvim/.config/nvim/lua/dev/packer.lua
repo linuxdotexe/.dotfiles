40f981b33b3a7099924346d981213ea3ccc6b648
Sai Nivas Mangu <sainonbeat99@gmail.com>
2023-02-01 19:05:26 +0530

    add: vim-be-good

--- nvim/.config/nvim/lua/dev/packer.lua
+++ nvim/.config/nvim/lua/dev/packer.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
 
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use ('theprimeagen/harpoon')
+  use ('theprimeagen/vim-be-good')
 
   -- LSP
   
--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -163,6 +163,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/dev/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["vim-be-good"] = {
+    loaded = true,
+    path = "/home/dev/.local/share/nvim/site/pack/packer/start/vim-be-good",
+    url = "https://github.com/theprimeagen/vim-be-good"
   }
 }
 
