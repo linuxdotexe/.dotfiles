f6cca187a05d4317c635d7906cc3415de4b87d17
Sai Nivas Mangu <sainonbeat99@gmail.com>
2023-02-03 16:22:35 +0530

    fix: remove unwanted lines

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,10 +2,6 @@
 
 filename=`ls $HOME/.appimages/*.AppImage`
 
-new_name='Execs=${!filename}' 
-
-# echo $filename
-
 sed -i '$ d' $HOME/.local/share/applications/remnote.desktop
 
 echo Exec=$filename >> $HOME/.local/share/applications/remnote.desktop
