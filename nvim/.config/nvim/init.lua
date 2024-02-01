977afcc15d5d2da6e8b2fe541488dd6091fe6484
sai nivas mangu <sainonbeat99@gmail.com>
2024-02-01 20:06:38 +0530

    add: remember cursor position func

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,3 +22,19 @@ vim.opt.scrolloff = 8
 
 vim.opt.updatetime = 50
 
+-- lifter from reddit
+-- remembers last place
+local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
+vim.api.nvim_clear_autocmds({ group = lastplace })
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = lastplace,
+    pattern = { "*" },
+    desc = "remember last cursor place",
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
